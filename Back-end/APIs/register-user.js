1eff604812f3bce27eac8a1c562bbeece0fda301
Arthur.Loni <arthur.loni@j2aconsultoria.com.br>
2026-08-29 20:17:48 -0400

    feat: :sparkles: Create Method HTTP
    Foi realizado a criação de dois metodos HTTP, GET x POST, para procurar e depois incluir um novo usuario

--- Back-end/APIs/register-user.js
+++ Back-end/APIs/register-user.js
@@ -6,18 +6,35 @@ app.use(express.json()); // Middleware para fazer o express entender o formato J
 
 let registerUser = [ // Criando um banco utilizando Array de objetos
     {id: 1,                              
-    name: "Arthur Loni",                 
-    email: "aarthurloniaiello@gmail.com",  
-    password:"1234"}                     
+    name: "admin",                 
+    email: "administrador@gmail.com",  
+    password:"admin"}                     
 ];
 
-// 1 - Primeira ROTA (Recebendo os usuarios | GET)
+// 1 - Primeiro Method (Recebendo os usuarios | GET)
 app.get('/api/user',(req,res) => {
     const registerNotPassword = registerUser.map(({ password, ...rest }) => rest); // Retirando senha dentro do array
     // res.json(registerNotPassword[0].id)
     // res.json(registerNotPassword[0].name)
     // res.json(registerNotPassword[0].email)
     res.json(registerNotPassword)
+});
+
+//2 - Segundo Method (Criando usuarios) | POST
+app.post('/api/create', (req, res) => {
+    const { name, email, password } = req.body; // Destruturando o body, senao seria usado dessa forma
+    // ex: const name = req.body.name;
+
+    // Valida se os campos obrigatorios vieram
+    if (!name || !email || !password) { // Operadores logicos, !=Not,||=Or
+        return res.status(400).json({ mensagem: 'Todos os campos são obrigatórios.' }); //add mensagem de erro com codigo http
+    }
+
+    const novoUsuario = { id: registerUser.length + 1, name, email, password }; // Tipo registerUser, utilizando length para ler numero de elementros
+    registerUser.push(novoUsuario); // Forma de adicionar no ultimo elemento
+
+    res.status(201).json(novoUsuario);
+    console.log("User criado com sucesso.")
 });
 
 // Inicializando servidor na porta configurada
